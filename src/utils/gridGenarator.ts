1402ffe4c1897d129a323ef3568059f40ad4bfcc
habibma <habib.motavassel@gmail.com>
2026-07-01 12:59:58 +0200

    to fix error

--- src/utils/gridGenarator.ts
+++ src/utils/gridGenarator.ts
@@ -2,13 +2,13 @@
 function generateRandomGrid(): string[][] {
     const size = Math.floor(Math.random() * 5) + 5; // a random grid size between 5 and 9
     const grid: string[][] = Array.from({ length: size }, () => Array(size).fill('.'))
-    grid.forEach((row, i) => {
-      row.forEach((cell, j) => {
-        if (Math.random() < 0.2) { // 20% chance to place an obstacle
-          grid[i][j] = '0' // '0' represents an obstacle
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (Math.random() < 0.3) { // 30% chance to place an obstacle
+                grid[i][j] = '0'
+            }
         }
-      })
-    })
+    }
     return grid
   }
 
